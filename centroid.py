bd77ac658c617770535fc4bbcc1d7858c245d663
diaz0329 <diaz0329@umn.edu>
2021-11-04 00:32:57 -0500

    Added red masking

--- centroid.py
+++ centroid.py
@@ -20,16 +20,20 @@ def bw_threshold(filepath):
     cv2.destroyAllWindows()
 
 def hsv_thresh(filepath):
-    ## mask of yellow (15,0,0) ~ (36, 255, 255)
     img = cv2.imread(filepath)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    ## mask of yellow (15,0,0) ~ (36, 255, 255)
+    # Modified values of S and V to avoid including background
+    yel_thresh = cv2.inRange(hsv_img, (15,90,90), (36,255,255)) 
+    ## mask of red (0, 0, 0) ~ (10, 255, 255) AND (170, 0, 0) ~ (180, 255, 255)
+    red_thresh1 = cv2.inRange(hsv_img, (0,120,120), (10,255,255))
+    red_thresh2 = cv2.inRange(hsv_img, (170,120,120), (180,255,255))
+    # Total mask for yellow and red colors
+    mask = yel_thresh + red_thresh1 + red_thresh2
+    #Converts binary image back into portions of original image
+    total_thresh = cv2.bitwise_and(img, img, mask=mask)
     cv2.imshow('orig', img)
-    #Modified values of S and V to avoid including background
-    thresh = cv2.inRange(hsv_img, (15,90,90), (36,255,255))
-    cv2.imshow('threshed', thresh)
-    #Converts binary image back into portions of original image 
-    yel_mask = cv2.bitwise_and(img, img, mask=thresh)
-    cv2.imshow('yells', yel_mask)
+    cv2.imshow('result', total_thresh)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -37,7 +41,7 @@ def hsv_thresh(filepath):
 def plot_point(filepath, x, y):
     img = cv2.imread(filepath)
     img = cv2.circle(img, (x, y), radius=10, color=(0, 0, 255), thickness=-1)
-    cv2.imshow('iwannadie', img)
+    cv2.imshow('plotted', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
